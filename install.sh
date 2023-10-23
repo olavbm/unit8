6df749f8245bccae73f43c9d4a024dbfdd73cb8e
olavbm <olavmarkussen@hotmail.no>
2023-10-23 14:38:22 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+sudo apt install -y \
+    python3-opengl \
+    ffmpeg \
+    xvfb \
+    swig \
+    cmake \
+    libsdl2-dev \
--- readme.md
+++ readme.md
@@ -1,8 +1,2 @@
 use python 3.10.12 with conda pref
-install:
-python-opengl
-ffmpeg
-xvfb
-swig 
-cmake
-libsdl2-dev
+use install.sh
